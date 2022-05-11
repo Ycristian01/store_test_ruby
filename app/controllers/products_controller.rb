40b337e7277d163abefae18d3dd2d5aa959dccda
Ycristian01 <ycristian@uninorte.edu.co>
2022-05-11 01:18:06 -0500

    created product CRUD

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,41 @@ class ProductsController < ApplicationController
   def show
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product
+      flash[:notice] = "Product was successfully updated." 
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    bought_product = BoughtProduct.where(product_id: @product.id)
+    if bought_product.empty?
+      @product.destroy
+      redirect_to products_url
+      flash[:notice] = "Product was successfully destroyed." 
+    else
+      redirect_to producy_url
+      flash[:notice] = "Product has been used. Cannot be destroyed."
+    end
+  end
+
   private
 
   def set_product
     @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:description, :price, :weight)
   end
 end
