3834202080c051d5e3b5cce38740b8f881997e11
Ycristian01 <ycristian@uninorte.edu.co>
2022-05-11 09:04:56 -0500

    created products crud

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,34 @@
 class OrdersController < ApplicationController
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
 
   def index
     @orders = current_user.orders
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @order = current_user.orders.build
+  end
+
+  def create
+    @order = current_user.orders.build(order_params)
+    if @order.save
+      redirect_to orders_path, notice: 'Order was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:number, :shipping_address_id, :payment_type)
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,6 +12,15 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: 'Product was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
@@ -43,6 +52,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:description, :price, :weight)
+    params.require(:product).permit(:name,:description, :price, :weight)
   end
 end
