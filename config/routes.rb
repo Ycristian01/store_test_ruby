59b99cb8c1a52694edc4f772ca66fc5cd2683658
Ycristian01 <ycristian@uninorte.edu.co>
2022-05-10 22:53:32 -0500

    fixed routes error and created devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   root 'stores#index'
   
   resources :stores
-  resources :users do
+  resources :users,  except:[:create, :show, :destroy, :edit, :update] do
     resources :shipping_addresses
   end
   resources :orders
   resources :products
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
